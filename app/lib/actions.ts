a9f5699ebfe5969464918af3e94c6ee7f8fe979a
Max Cheung <maxcheung07@gmail.com>
2025-01-26 16:41:15 -0800

    finished chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,13 +66,21 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+ const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Messing Fields, Failed to Update Invoice',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
@@ -82,7 +90,7 @@ export async function updateInvoice(id: string, formData: FormData) {
       WHERE id = ${id}
     `;
   } catch(error) {
-    console.error(error);
+    return { message: 'Database Error: Failed to Update Invoice.'};
   }
  
   revalidatePath('/dashboard/invoices');
